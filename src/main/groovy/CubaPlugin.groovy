9c678196937b75424873b2bfffc7fd2f5c1130dc
Konstantin Krivopustov <krivopustov@haulmont.com>
2012-09-07 10:35:12 +0000

    Refs #1204 WAR deployment

--- src/main/groovy/CubaPlugin.groovy
+++ src/main/groovy/CubaPlugin.groovy
@@ -376,6 +376,80 @@ class CubaDeployment extends DefaultTask {
     }
 }
 
+class CubaWarBuilding extends DefaultTask {
+
+    def appName
+    def Closure doAfter
+    def webcontentExclude = []
+    def dbScriptsExcludes = []
+    def tmpWarDir
+
+    CubaWarBuilding() {
+        setDescription('Builds WAR distribution')
+        tmpWarDir = "${project.buildDir}/tmp/war"
+    }
+
+    @TaskAction
+    def deploy() {
+        project.logger.info(">>> copying libs from configurations.runtime")
+        project.copy {
+            from project.configurations.runtime
+            from project.libsDir
+            into "${tmpWarDir}/WEB-INF/lib"
+            include { details ->
+                def name = details.file.name
+                return !(name.endsWith('-sources.jar'))
+            }
+        }
+
+        if (project.configurations.getAsMap().dbscripts) {
+            project.logger.info(">>> copying dbscripts from ${project.buildDir}/db to ${tmpWarDir}/WEB-INF/db")
+            project.copy {
+                from "${project.buildDir}/db"
+                into "${tmpWarDir}/WEB-INF/db"
+                excludes = dbScriptsExcludes
+            }
+        }
+
+        if (project.configurations.getAsMap().webcontent) {
+            def excludePatterns = ['**/web.xml'] + webcontentExclude
+            project.configurations.webcontent.files.each { dep ->
+                project.logger.info(">>> copying webcontent from $dep.absolutePath to ${tmpWarDir}")
+                project.copy {
+                    from project.zipTree(dep.absolutePath)
+                    into tmpWarDir
+                    excludes = excludePatterns
+                    includeEmptyDirs = false
+                }
+            }
+            project.logger.info(">>> copying webcontent from ${project.buildDir}/web to ${tmpWarDir}")
+            project.copy {
+                from "${project.buildDir}/web"
+                into tmpWarDir
+            }
+        }
+
+        project.logger.info(">>> copying from web to ${tmpWarDir}")
+        project.copy {
+            from 'web'
+            into tmpWarDir
+        }
+
+        if (doAfter) {
+            project.logger.info(">>> calling doAfter")
+            doAfter.call()
+        }
+
+        project.logger.info(">>> touch ${tmpWarDir}/WEB-INF/web.xml")
+        File webXml = new File("${tmpWarDir}/WEB-INF/web.xml")
+        webXml.setLastModified(new Date().getTime())
+
+        ant.jar(destfile: "${project.buildDir}/distributions/${appName}.war", basedir: tmpWarDir)
+
+        project.delete(tmpWarDir)
+    }
+}
+
 class CubaWebStartCreation extends DefaultTask {
 
     def jnlpTemplateName = "${project.projectDir}/webstart/template.jnlp"
