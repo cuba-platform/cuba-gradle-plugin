ed3526a687f462454c23bf8f9ea62ee13254c959
Yuriy Artamonov <artamonov@haulmont.com>
2013-04-04 12:28:27 +0000

    Fix LibraryVersionTest code-style

--- src/test/groovy/LibraryVersionTest.groovy
+++ src/test/groovy/LibraryVersionTest.groovy
@@ -2,9 +2,9 @@
  * @author hasanov
  * @version $Id$
  */
-class RegexesFinderTest extends GroovyTestCase {
+class LibraryVersionTest extends GroovyTestCase {
 
-    void testLibraryVersionsRegexTest() {
+    void testLibraryVersionMatcher() {
         def testData = [
                 "charts-global-4.0-SNAPSHOT.jar": ["charts-global", "4.0-SNAPSHOT"],
                 "asm-3.2-RELEASE.jar": ["asm", "3.2-RELEASE"],
@@ -16,24 +16,22 @@ class RegexesFinderTest extends GroovyTestCase {
                 "antlr-runtime-3.2.haulmont.jar": ["antlr-runtime", "3.2.haulmont"],
                 "core-renderer-R8-SNAPSHOT.jar": ["core-renderer", "R8-SNAPSHOT"],
                 "core-renderer-1.1.3.RELEASE.jar": ["core-renderer", "1.1.3.RELEASE"],
-                "core-lib-renderer2-3.1.haulmont-SNAPSHOT.jar" : ["core-lib-renderer2","3.1.haulmont-SNAPSHOT"],
-                "xpp3_min-1.1.4c.jar" : ["xpp3_min","1.1.4c"],
-                "some-lab.jar" : [null,null]
+                "core-lib-renderer2-3.1.haulmont-SNAPSHOT.jar": ["core-lib-renderer2", "3.1.haulmont-SNAPSHOT"],
+                "xpp3_min-1.1.4c.jar": ["xpp3_min", "1.1.4c"],
+                "some-lab.jar": [null, null]
         ]
 
         def resolver = new CubaDeployment.DependencyResolver()
         for (pair in testData) {
             def libraryName = pair.key
             def libraryDefinition = resolver.getLibraryDefinition(libraryName)
-            if (libraryDefinition != null){
+            if (libraryDefinition != null) {
                 assertEquals(pair.getValue().get(0), libraryDefinition.name)
                 assertEquals(pair.getValue().get(1), libraryDefinition.version)
+            } else {
+                assertNull(pair.getValue().get(0))
+                assertNull(pair.getValue().get(1))
             }
-            else {
-                assertEquals(libraryDefinition,pair.getValue().get(0))
-            }
-
-
         }
     }
 }
