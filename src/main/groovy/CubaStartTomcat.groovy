20dd2b07b9133c7e2d19dbd7aa8f56c91a3903c8
Yuriy Artamonov <artamonov@haulmont.com>
2013-11-02 11:52:19 +0000

    Environment variable for tomcat start on linux #PL-3043

--- src/main/groovy/CubaStartTomcat.groovy
+++ src/main/groovy/CubaStartTomcat.groovy
@@ -3,6 +3,8 @@
  * Use is subject to license terms, see http://www.cuba-platform.com/license for details.
  */
 
+
+import org.apache.commons.lang.StringUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -23,12 +25,25 @@ class CubaStartTomcat extends DefaultTask {
     def deploy() {
         def binDir = "${tomcatRootDir}/bin"
         project.logger.info ">>> starting $tomcatRootDir"
-        ant.exec(osfamily: 'windows', dir: "${binDir}", executable: 'cmd.exe', spawn: true) {
-            env(key: 'NOPAUSE', value: true)
-            arg(line: '/c start callAndExit.bat debug.bat')
-        }
-        ant.exec(osfamily: 'unix', dir: "${binDir}", executable: '/bin/sh') {
-            arg(line: 'debug.sh')
+
+        def tomcatStartScript = System.getenv("CUBA_TOMCAT_START_SCRIPT")
+        if (StringUtils.isBlank(tomcatStartScript)) {
+            ant.exec(osfamily: 'windows', dir: "${binDir}", executable: 'cmd.exe', spawn: true) {
+                env(key: 'NOPAUSE', value: true)
+                arg(line: '/c start callAndExit.bat debug.bat')
+            }
+            ant.exec(osfamily: 'unix', dir: "${binDir}", executable: '/bin/sh') {
+                arg(line: 'debug.sh')
+            }
+        } else {
+            println "Execute tomcat start with ${tomcatStartScript}"
+
+            ant.exec(osfamily: 'windows', dir: "${binDir}", executable: tomcatStartScript, spawn: true) {
+                arg(line: tomcatRootDir)
+            }
+            ant.exec(osfamily: 'unix', dir: "${binDir}", executable: tomcatStartScript) {
+                arg(line: tomcatRootDir)
+            }
         }
     }
 }
