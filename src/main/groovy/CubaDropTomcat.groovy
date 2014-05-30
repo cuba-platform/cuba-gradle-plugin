c164f7406815cccc01792dc17f4af59637286637
Yuriy Artamonov <artamonov@haulmont.com>
2014-05-30 07:21:48 +0000

    Fix task CubaDropTomcat #PL-3955

--- src/main/groovy/CubaDropTomcat.groovy
+++ src/main/groovy/CubaDropTomcat.groovy
@@ -27,23 +27,26 @@ class CubaDropTomcat extends DefaultTask {
             project.logger.info ">>> deleting $dir"
             // stop
             def binDir = "${tomcatRootDir}/bin"
-            ant.exec(osfamily: 'windows', dir: "${binDir}", executable: 'cmd.exe', spawn: true) {
-                env(key: 'NOPAUSE', value: true)
-                arg(line: '/c start callAndExit.bat shutdown.bat')
-            }
-            ant.exec(osfamily: 'unix', dir: "${binDir}", executable: '/bin/sh') {
-                arg(line: 'shutdown.sh')
-            }
-            // wait and delete
-            ant.waitfor(maxwait: 6, maxwaitunit: 'second', checkevery: 2, checkeveryunit: 'second') {
-                not {
-                    socket(server: 'localhost', port: listeningPort)
+
+            if (new File(binDir).exists()) {
+                ant.exec(osfamily: 'windows', dir: "${binDir}", executable: 'cmd.exe', spawn: true) {
+                    env(key: 'NOPAUSE', value: true)
+                    arg(line: '/c start callAndExit.bat shutdown.bat')
                 }
-            }
-            if (listeningPort) {
-                // kill to be sure
                 ant.exec(osfamily: 'unix', dir: "${binDir}", executable: '/bin/sh') {
-                    arg(line: "kill_by_port.sh $listeningPort")
+                    arg(line: 'shutdown.sh')
+                }
+                // wait and delete
+                ant.waitfor(maxwait: 6, maxwaitunit: 'second', checkevery: 2, checkeveryunit: 'second') {
+                    not {
+                        socket(server: 'localhost', port: listeningPort)
+                    }
+                }
+                if (listeningPort) {
+                    // kill to be sure
+                    ant.exec(osfamily: 'unix', dir: "${binDir}", executable: '/bin/sh') {
+                        arg(line: "kill_by_port.sh $listeningPort")
+                    }
                 }
             }
             project.delete(dir)
