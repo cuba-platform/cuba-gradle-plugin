ce1a291ad7e2404f4d374e94a9b83c139713d513
Yuriy Artamonov <artamonov@haulmont.com>
2014-08-28 13:30:15 +0000

    Fixed wait for HSQL start

--- src/main/groovy/CubaHsqlStart.groovy
+++ src/main/groovy/CubaHsqlStart.groovy
@@ -22,17 +22,9 @@ class CubaHsqlStart extends CubaHsqlTask {
         dbDataDir.mkdirs();
         if (GraphicsEnvironment.isHeadless()) {
             if ('linux'.equalsIgnoreCase(System.getProperty('os.name'))) {
-                // Use log files for startup on server
-
-                ProcessBuilder pb = new ProcessBuilder()
-                pb.command("java", "-cp", "$driverClasspath",
-                        "org.hsqldb.server.Server",
-                        "--database.0", "file:$dbName",
-                        "--dbname.0", "$dbName")
-                pb.redirectError(new File("hsql.error.log"))
-                pb.redirectOutput(new File("hsql.log"))
-                pb.directory(new File(dbDataDir.absolutePath))
-                pb.start()
+                ant.exec(dir: dbDataDir.absolutePath, executable: 'java', spawn: true) {
+                    arg(line: "-cp \"$driverClasspath\" org.hsqldb.server.Server --database.0 file:\"$dbName\" --dbname.0 \"$dbName\"")
+                }
             } else {
                 ant.exec(dir: dbDataDir.absolutePath, executable: 'cmd.exe', spawn: true) {
                     arg(line: "/C java.exe -cp $driverClasspath org.hsqldb.server.Server --database.0 file:$dbName --dbname.0 $dbName")
@@ -56,6 +48,10 @@ class CubaHsqlStart extends CubaHsqlTask {
                 arg(line: "$dbPort \"${dbDataDir.absolutePath}\" \"${dbName}\"")
             }
         }
-        Thread.sleep(1000)
+        ant.waitfor(maxwait: 10, maxwaitunit: 'second', checkevery: 1, checkeveryunit: 'second') {
+            not {
+                socket(server: 'localhost', port: "$dbPort")
+            }
+        }
     }
 }
