25e262711079b93ba100fd23d5cb1ec0f19b4a25
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-03-16 17:05:31 +0400

    PL-6894 Change copyright

--- src/main/groovy/CubaWarBuilding.groovy
+++ src/main/groovy/CubaWarBuilding.groovy
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2008-2016 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import org.apache.commons.lang.StringUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
@@ -5,8 +22,6 @@ import org.gradle.api.tasks.TaskAction
 
 /**
  *
- * @author degtyarjov
- * @version $Id$
  */
 class CubaWarBuilding extends DefaultTask {
     Project coreProject;
