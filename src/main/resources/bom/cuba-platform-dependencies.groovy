03e2b2a58ffc612e59182905ed9aa2372440b747
Yuriy Artamonov <artamonov@haulmont.com>
2016-11-15 13:14:51 +0400

    PL-8201 Timestamps of sources inside vaadin-server and vaadin-shared greater than class timestamps prevents dynamic compilation

--- src/main/resources/bom/cuba-platform-dependencies.groovy
+++ src/main/resources/bom/cuba-platform-dependencies.groovy
@@ -19,7 +19,7 @@ def springVersion = '4.3.3.RELEASE'
 def springSecurityVersion = '4.1.3.RELEASE'
 def thymeleafVersion = '3.0.2.RELEASE'
 def jacksonVersion = '2.8.0'
-def vaadinVersion = '7.7.3.cuba.2'
+def vaadinVersion = '7.7.3.cuba.3'
 def googleGwtVersion='2.8.0'
 
 def luceneVersion = '5.3.0'
