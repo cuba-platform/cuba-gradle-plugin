8e7ed29c0937a155f5b04e55cd9f196f5cd58bda
Andrey Subbotin <subbotin@haulmont.com>
2017-02-02 13:46:50 +0400

    PL-7068 Reporting Wizard generates incorrect attribute-aliases, if fields of assotiated entity are chosen to show in table

--- src/main/resources/bom/cuba-platform-dependencies.groovy
+++ src/main/resources/bom/cuba-platform-dependencies.groovy
@@ -28,7 +28,7 @@ def morphologyVersion = '1.1'
 def activitiVersion = '5.20.0'
 def modelerVersion = '5.20.0.cuba.18'
 
-def yargVersion = '1.1.1'
+def yargVersion = '1.1.3'
 
 [
   'org.apache.tomcat:tomcat' : '8.0.35',
