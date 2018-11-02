5748701acedd8fa85a37b09743a10908a6d975b5
Yuriy Artamonov <artamonov@haulmont.com>
2018-11-02 16:57:19 +0400

    Exception in CubaWebScssThemeCreation task if themes directory does not exist #76

--- src/main/groovy/CubaWebScssThemeCreation.groovy
+++ src/main/groovy/CubaWebScssThemeCreation.groovy
@@ -156,7 +156,7 @@ class CubaWebScssThemeCreation extends DefaultTask {
     void buildThemes() {
         def stylesDirectory = project.file(scssDir)
         if (!stylesDirectory.exists()) {
-            throw new FileNotFoundException("Unable to find SCSS themes root directory ${scssRoot.absolutePath}")
+            throw new FileNotFoundException("Unable to find SCSS themes root directory ${stylesDirectory.absolutePath}")
         }
 
         def themesTmp = new File(project.buildDir, "themes-tmp")
