1b1868e5505e73b902fc4f37edaecfe3eb4a8e40
Daniil Tsarev <tsarev@haulmont.com>
2019-12-06 16:24:32 +0400

    createDb task returns wrong message when someone is connected to a database #26

--- src/main/groovy/CubaDbCreation.groovy
+++ src/main/groovy/CubaDbCreation.groovy
@@ -125,10 +125,18 @@ class CubaDbCreation extends AbstractCubaDbCreation {
             }
         } catch (Exception e) {
             if (!isAcceptableException(e)) {
-                if (project.logger.isInfoEnabled()) {
-                    project.logger.info("[CubaDbCreation] Failed to execute SQL: $sql", e)
+                if (dbInUseError(e)) {
+                    if (project.logger.isInfoEnabled()) {
+                        project.logger.info("[CubaDbCreation] Failed to execute SQL: $sql\nDB is already in use")
+                    } else {
+                        project.logger.warn("[CubaDbCreation] Failed to execute SQL: $sql\nDB is already in use")
+                    }
                 } else {
-                    project.logger.warn("[CubaDbCreation] Failed to execute SQL: $sql\nError: $e.message")
+                    if (project.logger.isInfoEnabled()) {
+                        project.logger.info("[CubaDbCreation] Failed to execute SQL: $sql", e)
+                    } else {
+                        project.logger.warn("[CubaDbCreation] Failed to execute SQL: $sql\nError: $e.message")
+                    }
                 }
                 executed = false
             }
@@ -277,5 +285,17 @@ grant create session,
             default:
                 return false
         }
+    }
+
+    /**
+     * Checks that error occurred because someone is already connected to DB.
+     *
+     * @param ex exception
+     * @return true if error occurred due to existing connection
+     */
+    protected boolean dbInUseError(Exception ex) {
+        return ex instanceof SQLException &&
+                dbms == POSTGRES_DBMS &&
+                ((SQLException) ex).getSQLState() == '55006'
     }
 }
